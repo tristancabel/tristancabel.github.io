32b61008738051765fbd4efbdbf8ef19938b4ef8
tristan Cabel <tristan.cabel@inria.fr>
2016-04-03 10:13:15 +0200

    update figures

--- _posts/2016-04-02-Coursera-SF-crimes.md
+++ _posts/2016-04-02-Coursera-SF-crimes.md
@@ -14,7 +14,7 @@ In this assignment, we are trying to extract relevant data from the crimes occur
 We will find that crimes occured mostly during the evening and that most belong to the category **LARCENY/THEFT**. Furthermore, we will show that this type of crime is concentrated in a certain neighborhood.
 
 ### All Crimes 
-To start, let's look at the time distribution of all the crimes of a week. On the x-axis, we define a variable *HourOfWeek* as this : *Monday 00h00 = 0, Monday 01h00 = 1 , Monday 13h00 = 12 , Tuesday 00h00 = 24,* ...). 
+To start, let's look at the time distribution of all the crimes of a week. For the first following figure, on the x-axis, we define a variable *HourOfWeek* as this : *Monday 00h00 = 0, Monday 01h00 = 1 , Monday 13h00 = 12 , Tuesday 00h00 = 24,* ...). For the second following figure, we group by Hour of a day.
 
 <img src="{{ site.url }}/public/6_crime_vis/crimes_distribution_per_hour.png" width="900">
 
@@ -49,7 +49,9 @@ We can observe that this crime is highly concentrated in one neighborhood, even 
 Now let's see if this type of crime is occuring more often at specific times of the day and if it is distributed evenly during the week:
 <img src="{{ site.url }}/public/6_crime_vis/larceny_per_day.png" width="900">
 
+Here, as opposed to when we were considering all the crimes, we can clearly see that there is an increase of *Larceny/Theft* crimes on Fridays and week ends.
+
 <img src="{{ site.url }}/public/6_crime_vis/larceny_per_hour.png" width="900">
-On the first figure as opposed to when we were considering all the crimes, we can clearly see that there is an increase of *Larceny/Theft* crimes on Fridays and week ends. Moreover, the second figure tells us that most the crimes occured during the afternoon. Lastly, we notice that there is a decrease in crimes everyday just after lunch time. Nap time?
+Moreover, these last figures tells us that most the crimes occured during the afternoon. Lastly, we notice that there is a peak at lunch time.
 
 For reproductibility, here is the [script]({{site.url}}/public/6_crime_vis/crime_visu.py) used to create my pictures.
--- public/6_crime_vis/crime_visu.py
+++ public/6_crime_vis/crime_visu.py
@@ -10,9 +10,12 @@ import numpy   as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+
 # Plotting Options
 sns.set_style("whitegrid")
 sns.despine()
+
 
 def plot_bar(df, column, title, fname, hue):
     col_freq       = df.groupby(column).size()
@@ -34,26 +37,25 @@ def plot_by_day(df, column, title, fname, hue):
     bar   = col_freq.plot(kind='bar', x='DayOfWeek', y='count', title=title, fontsize=8, figsize=(12,8), stacked=False, width=1, color=color)
     bar.figure.savefig(fname)
     
-def periodOfDay(row):
+def periodOfDay(row, applyday=1):
     day_offset = {'Monday':0 , 'Tuesday':24, 'Wednesday':48, 'Thursday':72, 'Friday':96, 'Saturday':120, 'Sunday':144 }
     t = row['Time'].split(':')
-    res = day_offset[row['DayOfWeek']] + int(t[0])
+    res = day_offset[row['DayOfWeek']]*applyday + int(t[0])
     return res
 
 def plot_by_hour(df, title, fname, hue):
+    plt.close('all')
+    f, (ax1, ax2) = plt.subplots(2, 1)
+
     #group by every hour during a week.
-    #so Monday [00:00, 00:59] = 0; Monday [02:00,02:59] = 2;
-    df['HourOfWeek'] = df.apply (lambda row: periodOfDay(row),axis=1)
-
     col_freq = df.groupby('HourOfWeek').size()
-    color = sns.color_palette(hue, len(col_freq))
-    plt.figure()
-    line2d   = col_freq.plot(title=title, fontsize=8, figsize=(12,8), stacked=False, marker='o')
+    line = col_freq.plot(ax=ax1, title=title + ' distribution during a week', fontsize=8, stacked=False, marker='o', xticks=np.arange(0, 168, 12))
 
-    # major ticks every 12, minor ticks every 6
-    line2d.set_xticks(np.arange(0, 168, 12))
-    line2d.set_xticks( np.arange(0, 168, 6), minor=True)
-    line2d.figure.savefig(fname)
+    col_freq2 = df.groupby('Hour').size()
+    line2 = col_freq2.plot(ax=ax2, title=title + ' distribution during a day', fontsize=8, stacked=False, marker='o', xticks=np.arange(0, 24, 6))
+
+    f.subplots_adjust(hspace=.5)
+    f.savefig(fname)
     
 
 def plot_location(df, fname):
@@ -76,23 +78,34 @@ def plot_location(df, fname):
 
 
 def input_transformer(filepath):
-
+    """
+    Read file input and transform it into a pandas DataFrame
+    """
+    fp = open(filepath,'rb')
+    raw = fp.read().decode('utf-8')
+    return pd.read_csv(io.StringIO(raw), parse_dates=True, index_col=0, na_values='NONE')
 
 def main(filepath):
+    """
+    Script Entry Point
+    """
     print 'reading data'
-    fp = open(filepath,'rb').read().decode('utf-8')
-    df = pd.read_csv(io.StringIO(fp), parse_dates=True, index_col=0, na_values='NONE')
+    df = input_transformer(filepath)
     
-    #filter unused data
+    #filter unused data and add 2 new columns
     print 'filter data'
     df.drop(['Descript', 'PdDistrict', 'Address'],inplace=True,axis=1)
-
+    #so Monday [00:00, 00:59] = 0; Monday [02:00,02:59] = 2;
+    df['HourOfWeek'] = df.apply (lambda row: periodOfDay(row),axis=1)
+    df['Hour'] = df.apply (lambda row: periodOfDay(row, 0),axis=1)
+    
     #plot by crime category
     plot_bar(df, 'Category', 'Top Crime Categories', 'category.png', 'spectral')
 
     print 'plot by day'
     plot_by_day(df, 'DayOfWeek', 'crimes per day', 'crimes_per_day.png', 'muted')
-    plot_by_hour(df, 'crime distribution during a week', 'crimes_distribution_per_hour.png', 'pastel')
+    plot_by_hour(df, 'crime', 'crimes_distribution_per_hour.png', 'pastel')
+
     print "map all crimes"
     plot_location(df, 'map_all_crimes.png')
 
@@ -106,7 +119,9 @@ def main(filepath):
     #plot by time and day
     print 'plot Larceny by day'
     plot_by_day(df_larceny, 'DayOfWeek', 'Larceny crime per day', 'larceny_per_day.png', 'muted')
-    plot_by_hour(df_larceny, 'Larceny crime during a week', 'larceny_per_hour.png', 'pastel')
+    plot_by_hour(df_larceny, 'Larceny', 'larceny_per_hour.png', 'pastel')
+
+    #plt.show()
 
 
 if __name__ == '__main__':
